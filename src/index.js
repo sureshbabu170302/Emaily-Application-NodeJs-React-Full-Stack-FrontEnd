1786c3a5f7ec9a27f2fff047fd6aad85b4c220f0
SureshBabu170302 <sureshbabu.kanini@gmail.com>
2024-06-03 09:52:32 +0530

    Redux SETUP

--- src/index.js
+++ src/index.js
@@ -1,7 +1,15 @@
 import React from "react";
-import ReactDOM from 'react-dom/client';
+import ReactDOM from "react-dom/client";
 import App from "./Components/App";
+import { Provider } from "react-redux";
+import { createStore, applyMiddleware } from "redux";
+import reducers from "./reducers";
 
+const store = createStore(reducers, {}, applyMiddleware());
 const el = document.getElementById("root");
 const root = ReactDOM.createRoot(el);
-root.render(<App/>);
+root.render(
+    <Provider store={store}>
+    <App />
+    </Provider>
+);
